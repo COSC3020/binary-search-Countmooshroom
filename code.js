7d35171f4a1e2414172dceec41538b5584a0eea5
Countmooshroom <benjaminwatts314@gmail.com>
2023-09-13 16:19:53 -0600

    I wrote a binary search.

--- code.js
+++ code.js
@@ -1,3 +1,28 @@
 function binarySearch(list, element) {
-    return -1;
+    //Initialize variables
+    let low = 0;
+    let high = list.length;
+    let index = Math.floor(high / 2);
+    //Loop while searching
+    while (list[index] != element) {
+        //Check if item isn't in list
+        if (low + 1 == high) {
+            return -1;
+        }
+        //Element is lower
+        if (list[index] > element) {
+            high = index;
+        }
+        //Element is higher
+        else {
+            low = index;
+        }
+        //Find new midpoint
+        index = Math.floor((high + low) / 2);
+    }
+    return index;
 }
+
+//Test code
+/*list = [1, 2, 3, 4, 4, 5, 6, 7, 8, 9, 10, 11, 12, 12, 13, 14];
+console.log("Index: " + binarySearch(list, 2));*/
--- code.test.js
+++ code.test.js
@@ -5,8 +5,13 @@ eval(fs.readFileSync('code.js')+'');
 
 const testSearch =
     jsc.forall("array nat", function(arr) {
+        //I just put in this quick sort feature since I can assume the list is sorted.
+        //This code is based on https://www.w3schools.com/js/js_array_sort.asp
+        arr.sort(function(a, b){return a - b});
         if(arr.length > 0) {
-            return binarySearch(arr, arr[0]) === 0;
+            //I changed this line so that the code also succeeds if the item found
+            //is equal to the expected item (in case of duplicates)
+            return binarySearch(arr, arr[0]) === 0 || arr[0] == arr[binarySearch(arr, arr[0])];
         } else {
             return binarySearch(arr, "foo") === -1;
         }
